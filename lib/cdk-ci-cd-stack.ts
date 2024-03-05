8583fc4e7a3ef94fe1ccdcd6d5c80288be441902
aShunyaOtuski <shunya.otsuki@access-company.com>
2024-03-05 14:01:03 +0900

    add: lambda.Function

--- lib/cdk-ci-cd-stack.ts
+++ lib/cdk-ci-cd-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class CdkCiCdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCiCdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new lambda.Function(this, "CdkCiCdLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("src/lambda"),
+      handler: "index.handler",
+    });
   }
 }
